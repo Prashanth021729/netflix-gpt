377e323312b9856287179590b204867692be55b5
Prashanth_Chanagoni <136735366+Prashanth021729@users.noreply.github.com>
2024-05-27 18:30:34 -0400

    Update MainContainer.js

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -9,7 +9,7 @@ const MainContainer = () => {
     const{original_title, overview, id} = mainMovie
 
     return(
-        <div className="pt-[30%] pt-0">
+        <div className="pt-0">
             <VideoTitle title={original_title} overview ={overview}/>
             <VideoBackground movieid={id}/>
         </div>
